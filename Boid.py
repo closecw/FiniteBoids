ae02ee6db6643aee01a677d1316814195698c6a7
Carter Close <closecw@gmail.com>
2025-05-04 21:34:50 -0500

    Changed all 3 algo methods. Flocking achieved, fine tuning required.

--- Boid.py
+++ Boid.py
@@ -8,9 +8,8 @@ class Boid:
         self.turn_factor = turn_speed
 
     def separation(self, boids):
-        desired_separation = 2.0
+        desired_separation = 20.0
         steer = vec.Vec(0, 0)
-        #diff = vec.Vec(0, 0)
         count = 0
         for boid in boids:
             if boid is not self:
@@ -24,59 +23,50 @@ class Boid:
 
         if count > 0:
             steer /= count
-        if steer.magnitude() > 0:
-            steer.normalize()
+            if steer.magnitude() > 0:
+                steer.normalize()
             steer *= self.turn_factor
-            #steer -= self.velocity
-            steer.limit(self.turn_factor)
-            return steer
-        else:
-            return steer
+        steer.limit(max_speed)
+        return steer
 
     def alignment(self, boids):
-        prefer_distance = 5.0
-        velo_sum = vec.Vec(0, 0)
-        steer = vec.Vec(0, 0)
+        prefer_distance = 50
+        avg_vel = vec.Vec(0, 0)
         count = 0
         for boid in boids:
             if boid is not self:
                 dist = vec.Vec.distance(self.position, boid.position)
                 if 0 < dist < prefer_distance:
-                    velo_sum += boid.velocity
+                    avg_vel += boid.velocity
                     count += 1
 
         if count > 0:
-            avg = velo_sum / count
-            avg.normalize()
-            avg *= self.turn_factor
-            steer = avg - self.velocity
-            steer.limit(self.turn_factor)
+            avg_vel /= count
+            avg_vel.normalize()
+            avg_vel *= self.turn_factor
+            steer = avg_vel - self.velocity
+            steer.limit(max_speed)
             return steer
-        else:
-            return steer
+        return vec.Vec(0, 0)
 
     def cohesion(self, boids):
-        visible_distance = 7.5
+        visible_distance = 50
         center = vec.Vec(0, 0)
-        visible_positions = vec.Vec(0, 0)
-        steer = vec.Vec(0, 0)
         count = 0
         for boid in boids:
             if boid is not self:
                 dist = vec.Vec.distance(self.position, boid.position)
                 if 0 < dist < visible_distance:
-                    visible_positions += boid.position
+                    center += boid.position
                     count += 1
 
         if count > 0:
-            center = (visible_positions / count)
+            center /= count
             desired = center - self.position
             desired.normalize()
             desired *= self.turn_factor
-            steer.limit(self.turn_factor)
-            return steer
-        else:
-            return steer
+            return desired
+        return vec.Vec(0, 0)
 
     def avoid_walls(self, width, height, margin):
         steer = vec.Vec(0, 0)
@@ -97,7 +87,7 @@ class Boid:
         c_f = self.cohesion(boids)
         a_w_f = vec.Vec(0, 0)
         if width and height:
-            a_w_f = self.avoid_walls(self.position.x, self.position.y, margin)
+            a_w_f = self.avoid_walls(width, height, margin)
 
         self.velocity += s_f + a_f + c_f + a_w_f
         self.velocity.limit(max_speed)
